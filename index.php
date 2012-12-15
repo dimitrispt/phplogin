7ad670005bea95e623168e39bba979a8158e78e1
root <root@dimitris-ubuntu>
2012-12-15 12:41:02 +0200

    Login form added

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
 ?>
+
+
+<form method="POST" ACTION="login.php">
+
+user:<INPUT type="text" name="name" value="<?php echo $name ?>"><br />
+password:<INPUT type="password" name="password"><br />
+
+<input type="submit" value="SUBMIT">
+
+</form>
+
+
+<?php
+?>
